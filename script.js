7fb91b92341464c72bdf43a62e100e16c08a289d
Abe Mae <abemaesaavedra175@gmail.com>
2023-10-12 09:26:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+const navLinks = document.querySelectorAll('nav a');
+        
+        // Function to highlight the active link in the navigation bar
+        function setActiveLink(link) {
+            navLinks.forEach(link => link.classList.remove('active'));
+            link.classList.add('active');
+        }
